c5928369a5441fea57c5a7f85ec8ff5ae3905e79
Prajakt Shastry <prajakt.shastry@gmail.com>
2025-01-19 08:11:02 -0500

    Adding latest monitoring updates (#16)

--- dags/redshift_etl.py
+++ dags/redshift_etl.py
@@ -1,5 +1,4 @@
 from airflow import DAG
-from airflow.models import Variable
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 from airflow.providers.amazon.aws.transfers.s3_to_redshift import S3ToRedshiftOperator
 from airflow.providers.postgres.operators.postgres import PostgresOperator
